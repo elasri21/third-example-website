240dbbe2ced556f97ae330ff0236d686f77cc94b
elasri21 <islame.edu@gmail.com>
2021-01-24 15:14:41 +0100

    add all files

---
+++ main.js
@@ -0,0 +1,31 @@
+let socialMediaAcount = document.querySelectorAll('.footer .social a');
+socialMediaAcount.forEach(function(link){
+    link.setAttribute('target', '_blanck');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
